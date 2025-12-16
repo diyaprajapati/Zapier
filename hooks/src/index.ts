2791b8407d0e1d5c34ca74e575d84903f6b0d2f7
diyaprajapati <diyabprajapati2005@gmail.com>
2025-12-16 22:27:17 +0530

    create: new models, fix: version issues

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/client";
+
 const app = express();
+const client = new PrismaClient();
 
-// password logic
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
 
-    // store in db a new trigger
-    // push it on to queue (kafka/redis)
-    
+  //store in db a new trigger
+  await client.zapRun.create({
+    data: {
+        zapId: zapId,
+    }
+  })
+
+  //push it on to queue (kafka/redis)
+
 });
